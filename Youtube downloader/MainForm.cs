17e92e2569826482ede28f457c4f182f6077fba0
Rahul Anand <rahul1989@gmail.com>
2015-08-13 10:28:53 +0530

    Cleaning up

--- Youtube downloader/MainForm.cs
+++ Youtube downloader/MainForm.cs
@@ -170,7 +170,6 @@ namespace Youtube_downloader
                     
                     // Start the process with the info we specified.
                     exeProcess.Start();
-                    //_processID = exeProcess.Id;
                     exeProcess.BeginOutputReadLine(); //need to call this method to begin the event handling and generation from the process being run
                     exeProcess.BeginErrorReadLine();
                     //exeProcess.WaitForExit();   // calling WaitForExit() will suspend the UI thread. So don't do that.
